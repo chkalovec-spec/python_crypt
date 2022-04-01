721035351a61edfe4c1a28ea045d01f9d214a00c
Korev Dmitriy <korev.dmitriy@mail.ru>
2022-04-01 23:26:08 +0300

    pthon_crypt

---
+++ aes_crypt_encryption.py
@@ -0,0 +1,31 @@
+import pyAesCrypt
+import os
+
+def encryption(file, password):
+    buffer_size = 512 * 1024
+
+    pyAesCrypt.encryptFile(
+      str(file),
+      str(file) + '.crp',
+      password,
+      buffer_size
+    )
+
+    print (f'Файл {os.path.splitext(file)[0]} зашифрован')
+    os.remove(file)
+
+def walking_by_dirs (dir, password):
+  for name in os.listdir(dir):
+    path = os.path.join(dir, name)
+
+    if os.path.isfile(path):
+      try:
+        encryption(path, password)
+      except Exception as ex:
+          print(ex)
+     
+    else:
+      walking_by_dirs(path, password)
+
+password = input('Введите пароль для шифрования: ')
+walking_by_dirs(os.path.join('./files'), password)
